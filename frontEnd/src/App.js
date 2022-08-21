52057039a315f04d1bea48bf0fedac58ba540c05
Houssam Benhim <benhim.houssam@gmail.com>
2022-08-21 19:30:42 +0200

    first commit

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg'
-import './App.css'
+import './assets/css/App.css'
 
 function App() {
-  return <div className='App'>Hello</div>
+  return <div>Hello</div>
 }
 
 export default App
